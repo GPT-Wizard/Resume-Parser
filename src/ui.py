af1609177425df9517fd20a57bb95cb5d3aa41be
SSK-14 <sanjaykumar1481999@gmail.com>
2024-06-21 10:55:41 +0530

    Sanjay: update ui

--- src/ui.py
+++ src/ui.py
@@ -4,6 +4,11 @@ from src.model import model_options
 
 def sidebar():
     with st.sidebar:
+        st.title("Building a Generative AI Application: Resume Parser 📝🤖")
+        st.info(
+            "We will build a Generative AI application using a Large Language Model (LLM) to parse resumes."
+        )
+        st.markdown("---")
         st.header("Model Settings")
         st.sidebar.selectbox("Select a model", list(model_options.keys()), key="model_name")
         with st.expander("More settings"):
@@ -13,7 +18,9 @@ def sidebar():
             st.slider(
                 "Max Tokens", min_value=0, max_value=8000, value=1000, key="max_tokens"
             )
-
+        st.markdown("---")
+        st.image("src/assets/gpt-wizard.png")
+        st.image("src/assets/tw-logo.png")
 
 def file_uploader():
     uploaded_file = st.file_uploader("Upload Candidate Resume PDF", type="pdf")
